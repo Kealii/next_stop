dd5f1ea4c621a433a6893e3a12967371c002e11b
Michael Wong <michael.brian.wong@gmail.com>
2015-11-17 08:28:11 -0700

    styling and data displaying changes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,31 @@
 class WelcomeController < ApplicationController
   # NEXT_STOP = "http://nextstopapi-1507.herokuapp.com/api/v1"
   NEXT_STOP = "http://localhost:3001/api/v1"
+
   def index
     response = HTTParty.get("#{NEXT_STOP}/routes")
     @routes = JSON.parse(response.body).map do |route|
       Route.new(route['route_id'], route['route_short_name'])
+    end
+  end
+
+  def route
+    route_id = params[:route][:id]
+    response = HTTParty.get("#{NEXT_STOP}/routes/#{route_id}")
+    route_json = JSON.parse(response.body)
+    @route = Route.new(route_json['route_id'], route_json['route_short_name'])
+
+    @trips = route_json['trips'].first(5).map do |trip_json|
+      trip_response = HTTParty.get("#{NEXT_STOP}/trips/#{trip_json['trip_id']}")
+      stop_times = JSON.parse(trip_response.body)['stop_times'].map do |stop_time|
+        StopTime.new(stop_time['departure_time'], stop_time['name'], stop_time['stop_id'])
+      end
+
+      Trip.new(trip_json['trip_id'], stop_times, trip_json['service_id'])
+    end
+
+    @stops = route_json['stops'].map do |stop|
+      Stop.new(stop['stop_id'], stop['stop_name'])
     end
   end
 
@@ -17,28 +38,6 @@ class WelcomeController < ApplicationController
       r = Route.new(route_json['route_id'], route_json['route_short_name'])
       r.departure_time = route_json['departure_time']
       r
-    end
-
-  end
-
-  def route
-    route_id = params[:route][:id]
-    response = HTTParty.get("#{NEXT_STOP}/routes/#{route_id}")
-    route_json = JSON.parse(response.body)
-    @route = Route.new(route_json['route_id'], route_json['route_short_name'])
-
-    @trips = route_json['trips'].first(5).map do |trip_json|
-      trip_response = HTTParty.get("#{NEXT_STOP}/trips/#{trip_json['trip_id']}")
-      stop_times = JSON.parse(trip_response.body)['stop_times'].map do |stop_time|
-      # stop_times = trip_json['stop_times'].map do |stop_time|
-        StopTime.new(stop_time['departure_time'], stop_time['name'], stop_time['stop_id'])
-      end
-
-      Trip.new(trip_json['trip_id'], stop_times, trip_json['service_id'])
-    end
-
-    @stops = route_json['stops'].map do |stop|
-      Stop.new(stop['stop_id'], stop['stop_name'])
     end
   end
 end
