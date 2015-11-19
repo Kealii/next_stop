502599ee2e1b72c2f73594eed131f1b410ac440a
Michael Wong <michael.brian.wong@gmail.com>
2015-11-19 01:45:39 -0700

    adds vcr for stubbing http requests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,9 @@ require 'spec_helper'
 require 'rspec/rails'
 require 'simplecov'
 require 'database_cleaner'
+require 'vcr'
+require 'webmock/rspec'
+
 SimpleCov.start
 
 ActiveRecord::Migration.maintain_test_schema!
@@ -14,4 +17,22 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.include FactoryGirl::Syntax::Methods
+end
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/cassettes'
+  config.hook_into :webmock
+end
+
+RSpec.configure do |config|
+  # Add VCR to all tests
+  config.around(:each) do |example|
+    options = example.metadata
+    if options[:vcr]
+      name = example.metadata[:full_description].split(/\s+/, 2).join('/').underscore.gsub(/\./,'/').gsub(/[^\w\/]+/, '_').gsub(/\/$/, '')
+      VCR.use_cassette(name, &example)
+    else
+      VCR.turned_off(&example)
+    end
+  end
 end
